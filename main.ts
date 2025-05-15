011a4fe834ff20ae785e8a24b0d73369e009f5f7
analaura543 <anav79427@gmail.com>
2025-05-15 11:29:13 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
-basic.showIcon(IconNames.EighNote)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (input.isGesture(Gesture.TiltLeft)) {
         music.playMelody("C5 B A G A B C5 - ", 120)
-        basic.showString ("Feliz!")
-    } else if (input.isGesture (Gesture.TiltRight)) {
-        music.playMelody(" E D C D E E E - ", 100)
+        basic.showString("Feliz!")
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        music.playMelody("E D C D E E E - ", 100)
         basic.showString("Triste!")
     } else if (input.isGesture(Gesture.LogoDown)) {
         for (let index = 0; index < 4; index++) {
-        music.playTone(800, music.beat(BeatFraction.Eighth))
-        music.rets(music.beat(BeatFraction.Sixteenth))
+            music.playTone(800, music.beat(BeatFraction.Eighth))
+            music.rest(music.beat(BeatFraction.Sixteenth))
         }
         basic.showString("Techoo! ")
     } else {
-        basic.showString(IconNames.Heart)
+        basic.showIcon(IconNames.Heart)
         music.playTone(262, music.beat(BeatFraction.Whole))
         basic.pause(500)
     }
